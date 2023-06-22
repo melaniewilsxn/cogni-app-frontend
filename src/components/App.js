021cf3a72a46bdcf3fe3a5388164b2f302ffb3af
Melanie Wilson <melaniemwilson1@gmail.com>
2023-06-22 19:59:26 -0400

    updated app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom"
+import NavBar from "./NavBar"
+import Home from "./Home"
+import About from "./About"
+import TutorList from "./TutorList"
+import Contact from "./Contact"
+import TestimonialList from "Testimonial"
 
 function App() {
   return (
-    <div className="App">
-      <h1>App!</h1>
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/tutors">
+          <TutorList />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/testimonials">
+          <TestimonialList />
+        </Route>
+      </Switch>
     </div>
   );
 }
