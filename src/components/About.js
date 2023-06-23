efbf9f8eb7ba4f4ff27066c4672011cf92c50a01
Melanie Wilson <melaniemwilson1@gmail.com>
2023-06-22 21:22:40 -0400

    added home and about component text

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,27 @@ import React from "react"
 function About(){
     return(
         <div>
-            <h2>About component</h2>
+            <h1>Meet Dr. Sophia Reynolds</h1>
+            <p>Dr. Sophia Reynolds, the visionary founder and CEO of Cogni, is a true advocate for empowering students to reach their full potential. With a profound passion for education and a deep understanding of the challenges faced by students in the modern learning landscape, Dr. Reynolds embarked on a mission to revolutionize the tutoring industry.</p>
+            <p>Driven by her own experiences as a student and her unwavering belief in the transformative power of personalized learning, Dr. Reynolds embarked on a journey to create Cogni. Having witnessed firsthand the struggles faced by students who were falling through the cracks of traditional education systems, she recognized the need for a holistic approach that catered to individual needs and fostered a love for learning.</p>
+            <p>As a former educator and researcher, Dr. Reynolds holds a Ph.D. in Educational Psychology. Her extensive knowledge in the field, coupled with her innovative thinking, led her to envision a tutoring platform that combined the best of technology and human expertise. She wanted to create a space where students could receive one-on-one attention, customized learning plans, and access to a network of highly qualified tutors passionate about making a difference.</p>
+            <p>Driven by her unwavering commitment to educational equity and a vision of empowering every student to thrive academically, Dr. Reynolds assembled a team of dedicated educators and technologists to bring Cogni to life. Together, they designed a cutting-edge platform that seamlessly integrates advanced learning algorithms with personalized guidance, fostering a nurturing environment where students can grow, build confidence, and achieve remarkable academic success.</p>
+            <p>Under Dr. Reynolds' leadership, Cogni has become a trusted name in the education industry, positively impacting countless students' lives and transforming the way tutoring is approached. Through her tireless dedication and unwavering commitment to excellence, Dr. Sophia Reynolds continues to shape the future of education, one student at a time.</p>
+            <h1>Our Approach</h1>
+            <p>At Cogni, we believe that every student has unique learning needs and deserves personalized attention to unlock their full potential. Our approach is centered around creating an engaging and supportive learning environment that fosters academic growth, builds confidence, and ignites a lifelong passion for learning. Here's how we do it:</p>
+            <h3>Personalized Learning Plans</h3>
+            <p>We understand that one size does not fit all when it comes to education. That's why we create customized learning plans for each student, tailored to their specific strengths, weaknesses, and learning style. Our experienced tutors take the time to assess each student's needs and goals, allowing us to develop a roadmap that maximizes their learning outcomes.</p>
+            <h3>Expert Tutors</h3>
+            <p>Our team of expert tutors is the heart and soul of Cogni. They are not only highly qualified and experienced in their respective subjects but also deeply committed to nurturing students' academic growth. Our tutors go beyond textbook teaching; they serve as mentors, motivators, and guides, providing individualized attention and support to help students succeed.</p>
+            <h3>Interactive Learning</h3>
+            <p>Learning should be an interactive and engaging experience. We incorporate interactive tools, multimedia resources, and real-world examples to make learning exciting and relatable. Our tutors use a variety of teaching techniques to ensure that students grasp concepts effectively, fostering a deeper understanding of the subject matter.</p>
+            <h3>Continuous Progress Monitoring</h3>
+            <p>We believe that progress should be measured and celebrated. Through regular assessments and progress tracking, we monitor each student's advancement, identifying areas of improvement and celebrating their milestones along the way. This enables us to make data-driven decisions, adapt our teaching strategies, and ensure consistent growth.</p>
+            <h3>Building Confidence and Motivation</h3>
+            <p>Academic success is not just about knowledge; it's about building confidence and motivation. We strive to create a supportive and nurturing environment where students feel empowered to take on challenges, embrace learning, and overcome obstacles. By instilling a growth mindset and celebrating small victories, we help students develop the resilience and self-belief necessary for long-term success.</p>
+            <h3>Parent Collaboration</h3>
+            <p>We believe in the power of collaboration between tutors, students, and parents. We actively involve parents in their child's educational journey by providing regular progress updates, open lines of communication, and guidance on how to support their child's learning outside of tutoring sessions. Together, we form a strong partnership to ensure students receive the best possible educational support.</p>
+            <p>At Cogni, we are dedicated to transforming education through personalized tutoring, fostering a love for learning, and equipping students with the skills they need to thrive academically and beyond. Join us on this exciting journey and unlock your full potential with Cogni!</p>
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,8 @@ import React from "react"
 function Home(){
     return(
         <div>
-            <h2>Home component</h2>
+            <h1>Unlocking Academic Success Through Personalized Tutoring.</h1>
+            <img src="https://images.unsplash.com/photo-1532619187608-e5375cab36aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="tutor-image-1"/>
         </div>
     )
 }
